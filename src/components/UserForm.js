c28a6fb775e8f057a7a3c1884a4d81ba63d73ea9
Ellyn Golden <ejgolden94@gmail.com>
2021-05-23 14:26:05 -0400

    login route working and will redirect to home, user is stored in state

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react' 
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Container, Form, Button, Header, Message } from 'semantic-ui-react'
 
 export default class UserForm extends Component {
@@ -9,38 +9,84 @@ export default class UserForm extends Component {
             username: '',
             email:'',
             password: '',
+            loginSuccess: false, 
+            message: ''
         }
+
+        this.context = props.context
     }
     
     handleChange = (event) => {
         this.setState({[event.target.id]:event.target.value})
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault()
-        console.log(this.state);
+        let url=''
+        if(this.props.context === 'login'){
+            url = this.props.baseURL +'/users/login'
+        }else{
+            url = this.props.baseURL +'/users/register'
+        }
+
+        let body = JSON.stringify(this.state)
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: body,
+            credentials: 'include'
+        }
+
+        const authUser = await fetch(url,requestOptions).then(response => response.json())
+        if(authUser.status === 200){
+            this.props.setCurrentUser(authUser.data)
+            this.setState({
+                loginSuccess: true
+            })
+        } else {
+            this.setState({
+                message: authUser.message
+            })
+        }
     }
 
     render(){
+        if (this.state.loginSuccess) {
+            return <Redirect to='/' />
+          }
         return (
             <Container style={{height:'100vh', textAlign:'center'}}>
-                <Header as='h1' color='teal' size='huge' textAlign='center' style={{width: '50%', margin:'30vh auto 20px auto'}}>
+                <Header as='h1' color='teal' size='huge' textAlign='center' style={{width: '50%', margin:'30vh auto 20px auto', fontSize:'calc(10px + 3vmin)'}}>
                 {this.props.context === 'login'?
                     <> Login </>:
                     <> Sign Up </>
                     }
                 </Header>
-                <Form size='large' style={{width: '50%', margin:'20px auto'}} onSubmit={(event)=>this.handleSubmit(event)}>
+                <Form size='large' style={{width: '70%', margin:'20px auto'}} onSubmit={(event)=>this.handleSubmit(event)}>
                     <Form.Input 
                         fluid 
-                        icon='user' 
-                        name='username'
-                        id='username'
+                        icon='mail' 
+                        name='email'
+                        id='email'
                         iconPosition='left' 
-                        placeholder='Username' 
-                        value={this.state.username}
+                        placeholder='User Email' 
+                        value={this.state.email}
                         onChange={(event)=>this.handleChange(event)}
                         />
+                    {this.props.context === 'signup'?
+                    <Form.Input
+                        fluid
+                        icon='user'
+                        name='username'
+                        id='username'
+                        iconPosition='left'
+                        value={this.state.username}
+                        placeholder='Username'
+                        type='text'
+                        onChange={(event)=>this.handleChange(event)}
+                    /> : ''
+                    }
                     <Form.Input
                         fluid
                         icon='lock'
@@ -61,13 +107,13 @@ export default class UserForm extends Component {
                 </Form>
                 {/* User login Message */}
                 {this.props.context === 'login'?
-                <Message color='orange' style={{width: '50%', margin:'0 auto'}}>
+                <Message color='orange' style={{width: '70%', margin:'0 auto'}}>
                     No Login? <Link to='/user/new'>Sign Up!</Link>
                 </Message>:null
                 }
                 {/* User Sign Up Message */}
                 {this.props.context === 'signup'?
-                <Message color='orange' style={{width: '50%', margin:'0 auto'}}>
+                <Message color='orange' style={{width: '70%', margin:'0 auto'}}>
                     Already have an account? <Link to='/user/login'>Sign In!</Link>
                 </Message>:null
                 }
