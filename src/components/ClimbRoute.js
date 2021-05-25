f4807a8ad2a80cfcfb4bdd036cfe01f20aee4d36
Ellyn Golden <ejgolden94@gmail.com>
2021-05-25 19:20:24 -0400

    wrapping climb routes in a link

--- src/components/ClimbRoute.js
+++ src/components/ClimbRoute.js
@@ -5,21 +5,22 @@ import {Link} from 'react-router-dom'
 export default function ClimbRoute (props) {
     const {route} = props
     return (
-
-    <Segment style={{width:'60%', margin:'25px auto' }}>
-        <div style={{display: 'flex', alignItems:'center', justifyContent: 'center'}}>
-            <Image
-                floated='left'
-                size='small'
-                rounded
-                src='https://ptrenew.com/wp-content/uploads/Screen-Shot-2019-04-05-at-1.35.48-PM.png'
-                />
-            <div style={{textAlign:'left'}}>
-                <Header>{route.name}</Header>
-                <div>{route.description.slice(0,100)+'...'}</div>
-                <Icon name='fire' style={{marginTop:'10px'}}/>6 climbs Logged
+        <Link to={'/routes/'+route.id} style={{color:'inherit'}} >
+        <Segment style={{width:'60%', margin:'25px auto' }}>
+            <div style={{display: 'flex', alignItems:'center', justifyContent: 'center'}}>
+                <Image
+                    floated='left'
+                    size='small'
+                    rounded
+                    src='https://ptrenew.com/wp-content/uploads/Screen-Shot-2019-04-05-at-1.35.48-PM.png'
+                    />
+                <div style={{textAlign:'left'}}>
+                    <Header>{route.name}</Header>
+                    <div>{route.description.slice(0,100)+'...'}</div>
+                    <Icon name='fire' style={{marginTop:'10px'}}/>6 climbs Logged
+                </div>
             </div>
-        </div>
-    </Segment>
+        </Segment>
+        </Link>
     )
 }
