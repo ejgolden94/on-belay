f61fa448c6eeee2cd7485721e9e9a1085f696c47
Ellyn Golden <ejgolden94@gmail.com>
2021-05-25 21:15:08 -0400

    404 component created

--- src/components/ClimbRoute.js
+++ src/components/ClimbRoute.js
@@ -1,10 +1,37 @@
-import React from 'react' 
-import {Card, Icon, Image, Segment, Header} from 'semantic-ui-react'
-import {useLocation} from 'react-router-dom'
+import React, {useState, useEffect} from 'react' 
+import NotFound from './NotFound'
+import {Button, Header, Container, Image} from 'semantic-ui-react'
+import {Link, useLocation} from 'react-router-dom'
 
 export default function ClimbRoute (props){
     const location = useLocation();
     const locationId = location.pathname.split('/')[2]
+
+    const {baseURL} = props
+    const [route, setRoute] = useState([])
+
+    const getRoute = async() => {
+        const url = baseURL + '/routes/' + locationId
+        const requestOptions = {
+          method:'GET',
+          credentials: 'include'
+        }
+    
+        let foundRoute = await fetch(url,requestOptions).then(response => response.json())
+        console.log(foundRoute);
+        setRoute(foundRoute)
+    }
+
+    useEffect(()=> {
+        getRoute()
+    },[])
+
+    // console.log(route);
+    if(route.status === 404){
+        return (
+            <NotFound redirect='Routes' redirectTo='/routes'/>
+        )
+    }
     return(
         <Header>Hey {locationId}</Header>
     )
