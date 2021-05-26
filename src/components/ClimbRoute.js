d657e544d46d67bc5e8acbe72aa5a9141e9b862e
Ellyn Golden <ejgolden94@gmail.com>
2021-05-25 23:31:23 -0400

    created routes page plus place holders

--- src/components/ClimbRoute.js
+++ src/components/ClimbRoute.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react' 
 import NotFound from './NotFound'
-import {Button, Header, Container, Image} from 'semantic-ui-react'
+import {Header, Image, Icon, Segment, Divider} from 'semantic-ui-react'
 import {Link, useLocation} from 'react-router-dom'
 
 export default function ClimbRoute (props){
@@ -26,13 +26,70 @@ export default function ClimbRoute (props){
         getRoute()
     },[])
 
-    // console.log(route);
+    console.log(route);
     if(route.status === 404){
         return (
             <NotFound redirect='Routes' redirectTo='/routes'/>
         )
+    } else {
+        return(
+            route.data?
+            <>
+            <Link to='/routes' style={{color:'inherit'}}>
+                    <Icon 
+                    circular inverted 
+                    color='teal' 
+                    name='angle left' 
+                    size='large' 
+                    className='float-left'
+                    style={{zIndex:'100'}}
+                    />
+            </Link>
+            <Segment style={{maxWidth: '80%', margin: '0 auto 5vh auto', border:'none', padding: '0'}}>
+                <Header style={{margin:'2vh', fontSize:'2.5em', fontWeight:'900'}}>
+                    {route.data.name}
+                </Header>
+                <Image 
+                    src={route.data.image? route.data.image:'/climb-route-stock.jpeg' } 
+                    style={{width: '100%', height:'30vh', objectFit: 'cover', margin: '0 auto'}}
+                />
+                <Segment className='stats' style={{backgroundColor:'lightgray', margin:'0 0 2vh 0'}}>
+                    <div>Your Stats:</div>
+                    <div>--</div>
+                    <div>--</div>
+                    <div>--</div>
+                </Segment>
+                <Divider horizontal>
+                    <Header as='h3'>
+                        Description
+                    </Header>
+                </Divider>
+                <p>{route.data.description}</p>
+                <Divider horizontal>
+                    <Header as='h3'>
+                        Comments
+                    </Header>
+                </Divider>
+                {/* add comment s component right here */}
+                    <Segment rounded  style={{display:'flex', width:'80%', margin:'0 auto'}}>
+                        <Segment circular style={{height:'75px', width: '75px'}}>User 1</Segment>
+                        <Segment style={{border:'none', boxShadow:'none'}}>I have a lot to say a bout this route...</Segment>
+                    </Segment>
+                    <Segment rounded  style={{display:'flex', width:'80%', margin:'20px auto'}}>
+                        <Segment circular style={{height:'75px', width: '75px'}}>User 2</Segment>
+                        <Segment style={{border:'none', boxShadow:'none'}}>Same...</Segment>
+                    </Segment>
+                <Divider horizontal>
+                    <Header as='h3'>
+                        Your Climbs
+                    </Header>
+                </Divider>
+                    <Segment rounded  style={{display:'flex', width:'80%', margin:'0 auto'}}>
+                        <Segment style={{border:'none', boxShadow:'none'}}>climb climb climb</Segment>
+                    </Segment>
+            </Segment>
+            </>
+            :''
+        )
     }
-    return(
-        <Header>Hey {locationId}</Header>
-    )
 }
