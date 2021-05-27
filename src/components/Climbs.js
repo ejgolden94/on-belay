aed29a8730fb3421a52eb751cd9f301750772506
Ellyn Golden <ejgolden94@gmail.com>
2021-05-27 10:12:53 -0400

    climb cards are rendering on climbs page

--- src/components/Climbs.js
+++ src/components/Climbs.js
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from 'react' 
-import {Card, Icon} from 'semantic-ui-react'
+import {Card, Container, Icon} from 'semantic-ui-react'
 
 export default function Climbs(props) {
     const {baseURL} = props
     const [climbs, setClimbs] = useState([])
 
+    const convertDate = (dateString) => {
+        const d = new Date(dateString)
+        const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+        return d.toLocaleDateString('en',dateOptions)
+    }
     useEffect(() => {
         const getClimbs = async() => {
             const url = baseURL+'/climbs/'
@@ -19,16 +24,30 @@ export default function Climbs(props) {
         getClimbs()
     },[])
 
-      return (
-        <ul>
-            {climbs? climbs.map(climb => 
-            <li key={climb.id}>
-                <Icon
-                    name='lightning'
-                    color='yellow'
-                />
-                {climb.performance}
-            </li>): ''}
-        </ul>
-      )
+    // created_date = new Date(climb.created)
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    console.log(climbs);
+    return (
+        <Container style={{margin:'50px auto'}}>
+        {climbs? climbs.map(climb => 
+            <Card key={climb.id} className='climbcard'>
+                <Card.Header>{climb.route.name}</Card.Header>
+                <Card.Content className='climbcard-content'>
+                    <div className='circle' style={{height:'50px', width:'50px'}}>
+                        {climb.route.rating}
+                    </div>
+                    <div>
+                        {convertDate(climb.created)}
+                        <div>
+                            <Icon
+                                name='lightning'
+                                color='yellow'
+                            />
+                            {climb.performance}
+                        </div>
+                    </div>
+                </Card.Content>
+            </Card>): ''}
+        </Container>
+    )
 }
