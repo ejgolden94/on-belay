33e48c208eb747eaef8259825a745c7fcb239ef4
Ellyn Golden <ejgolden94@gmail.com>
2021-05-30 12:59:32 -0700

    deleting a route is harder

--- src/components/RouteSidebar.js
+++ src/components/RouteSidebar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react' 
-import {Button, Menu, Sidebar, Icon} from 'semantic-ui-react'
+import {Button, Menu, Sidebar, Icon, Modal} from 'semantic-ui-react'
 import {Redirect, Link} from 'react-router-dom'
 
 export default function RouteSidebar (props){
@@ -7,9 +7,14 @@ export default function RouteSidebar (props){
     const {baseURL, route} = props
     const [visible, setVisible] = useState(false)
     const [deleted, setDeleted] = useState(false)
+    const [modalOpen, setModalOpen] = useState(false)
 
     const toggleVisible = () => {
         setVisible(!visible)
+    }
+
+    const toggleModalOpen = () => {
+        setModalOpen(!modalOpen)
     }
 
     const deleteRoute = async() => {
@@ -43,14 +48,41 @@ export default function RouteSidebar (props){
                 <Icon name='pencil' style={{margin:'1vh auto'}}/>
                 Edit
                 </Menu.Item>
-                <Menu.Item onClick={()=>deleteRoute()}>
+                <Menu.Item onClick={()=>toggleModalOpen()}>
                 <Icon name='delete' style={{margin:'1vh auto'}}/>
                 Delete
+                </Menu.Item>
+                <Menu.Item>
+                <Icon name='flag' style={{margin:'1vh auto'}}/>
+                Flag For Abuse
+                </Menu.Item>
+                <Menu.Item>
+                <Icon name='announcement' style={{margin:'1vh auto'}}/>
+                Make an Announcement
                 </Menu.Item>
             </Sidebar>
             <Button icon inverted className='menu-btn' onClick={()=>toggleVisible()}>
                 <Icon name='sidebar' />
             </Button>
+
+
+            <Modal
+                size='mini'
+                open={modalOpen}
+                >
+                <Modal.Header>Delete This Route</Modal.Header>
+                <Modal.Content>
+                <p>Hey! Are you Sure you would like to delete this Route?</p>
+                </Modal.Content>
+                <Modal.Actions>
+                <Button negative onClick={() => toggleModalOpen()}>
+                    No
+                </Button>
+                <Button positive onClick={() => deleteRoute()}>
+                    Yes
+                </Button>
+                </Modal.Actions>
+            </Modal>
             </>
         )
         }
