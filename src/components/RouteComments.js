0c3a33e89dfe10a52d2cdedef53fa751ba44b95e
Ellyn Golden <ejgolden94@gmail.com>
2021-05-31 11:04:35 -0700

    can collapse and see all comments

--- src/components/RouteComments.js
+++ src/components/RouteComments.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react' 
-import {Comment, Rating} from 'semantic-ui-react'
+import {Button, Comment, Rating} from 'semantic-ui-react'
 import {formatDate} from '../formatDate'
 
 export default function RouteComments(props){
     const {baseURL, routeId} = props
     const [comments, setcomments] = useState([]);
+    const [seeAll, setSeeAll] = useState(false)
 
     useEffect(()=>{
         const getRouteComments = async() => {
@@ -21,10 +22,14 @@ export default function RouteComments(props){
 
 
     console.log(comments)
+    let limit = 2
+    if(seeAll){ limit = comments.length }
+    console.log(comments.slice(0,limit))
     return(
-        comments.map(comment => {
+        <>
+        {comments.slice(0,limit).map(comment => {
             return(
-            <Comment>
+            <Comment style={{textAlign:'left'}}>
             <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
             <Comment.Content>
                 <Comment.Author>{comment.creator.username}</Comment.Author>
@@ -34,6 +39,10 @@ export default function RouteComments(props){
                 <Comment.Text>{comment.text}</Comment.Text>
             </Comment.Content>
             </Comment>
-        )})
+        )})}
+        {!seeAll?
+        <Button size='mini' color='purple' inverted onClick={()=>setSeeAll(true)}>See All Comments</Button> : 
+        <Button size='mini' color='purple' inverted onClick={()=>setSeeAll(false)}>Collapse Comments</Button> }
+        </>
     )
 }
