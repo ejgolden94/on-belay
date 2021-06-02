1a6db3e02f1b1d7a81c7bef25f50abcd66dbf746
Ellyn Golden <ejgolden94@gmail.com>
2021-06-02 19:40:13 -0400

    rating class now supports v scale

--- src/calculateRatingClass.js
+++ src/calculateRatingClass.js
@@ -18,5 +18,29 @@ export function calculateRatingClass (rating) {
     } else {
         return '#333333'
     }
+  } else if (rating[0]==='V'){
+    rating = rating.slice(1)
+    if (rating === 'B'){
+        return '#0D2E41'
+    } else{
+        rating = parseInt(rating)
+        if (rating === 0){
+            return '#00798A'
+        } else if (rating === 1){ 
+            return '#44AD9F'
+        } else if (rating === 2 || rating === 3){
+            return '#9EAE66'
+        } else if (rating >= 4 && rating <= 6){
+            return '#F8AF2C'
+        } else if (rating >= 7 && rating <= 9){
+            return '#F98830'
+        } else if (rating >= 10 && rating <= 16){  
+            return '#FA6135'
+        } else {
+            return '#333333'
+        }
+    }
+  } else {
+    return '#333333'
   }
 }
